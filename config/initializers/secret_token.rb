c7c9ecd6960d1e997c3f00424baa26e96ba6a309
epdejager <eliza.dejager@statpro.com>
2013-09-11 15:04:19 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatarangepickerTest::Application.config.secret_token = '5819c3ffda60fa2b47199c3291189c49199d0034161e76224d0c8dc10e53ac8817bb147b2bb7e571ae039f2303ef15860bf604f914e557bc4793a273bfd34e67'
